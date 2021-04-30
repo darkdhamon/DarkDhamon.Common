5fca117644e665d7f5ea76aaef8c3ee58c100d11
Bronze Brown <dhamon.nightstar@gmail.com>
2021-04-30 06:48:29 -0400

    Add ApiResponse Objects

---
+++ DarkDhamon.Common/API/Models/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace DarkDhamon.Common.API.Models
+{
+    public class ApiResponse
+    {
+        public string Message { get; set; }
+    }
+
+    public class ApiResponse<T>:ApiResponse
+    {
+        public T Data { get; set; }
+    }
+}
---
+++ DarkDhamon.Common/API/Models/PagedApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DarkDhamon.Common.API.Models
+{
+    public class PagedApiResponse<T> : ApiResponse
+    {
+        public IEnumerable<T> Data { get; set; }
+        public int Page { get; set; }
+        public int NumPerPage { get; set; }
+    }
+}
